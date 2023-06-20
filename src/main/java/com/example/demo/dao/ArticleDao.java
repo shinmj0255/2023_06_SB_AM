534c81dc6cdb02ad327235188d7d2ee1ffa04d89
shinmj0255 <shinmj0255@gmail.com>
2023-06-20 18:56:23 +0900

    ArticleDao.xml, 동적SQL

--- src/main/java/com/example/demo/dao/ArticleDao.java
+++ src/main/java/com/example/demo/dao/ArticleDao.java
@@ -1,13 +1,8 @@
 package com.example.demo.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
-import org.springframework.stereotype.Component;
 
 import com.example.demo.vo.Article;
 
@@ -16,32 +11,11 @@ public interface ArticleDao {
 	
 	public Article writeArticle(String title, String body);
 
-	@Select("""
-			SELECT * 
-				FROM article 
-				WHERE id = #{id}
-			""")
 	public Article getArticleById(int id);
-
-	@Update("""
-			UPDATE article 
-				SET updateDate = NOW()
-					, title = #{title}
-					, `body` = #{body} 
-				WHERE id = #{id}
-			""")
+	
 	public void modifyArticle(int id, String title, String body);
 
-	@Delete("""
-			DELETE FROM article 
-				WHERE id =#{id}
-			""")
 	public void deleteArticle(int id);
 
-	@Select("""
-			SELECT * 
-				FROM article 
-				ORDER BY id DESC
-			""")
 	public List<Article> getArticles();
 }
