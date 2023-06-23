8c3b6a0d9d91eb76ae1e6ed80042f44288775358
shinmj0255 <shinmj0255@gmail.com>
2023-06-23 20:19:07 +0900

    doAdd에 작성자 정보 저장, getLastInsertId return값 수정

--- src/main/java/com/example/demo/dao/ArticleDao.java
+++ src/main/java/com/example/demo/dao/ArticleDao.java
@@ -9,7 +9,7 @@ import com.example.demo.vo.Article;
 @Mapper
 public interface ArticleDao {
 	
-	public void writeArticle(String title, String body);
+	public void writeArticle(int memberId, String title, String body);
 
 	public Article getArticleById(int id);
 	
@@ -19,5 +19,5 @@ public interface ArticleDao {
 
 	public List<Article> getArticles();
 
-	public void getLastInsertId();
+	public int getLastInsertId();
 }
