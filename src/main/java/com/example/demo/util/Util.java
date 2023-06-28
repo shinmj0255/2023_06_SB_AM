620983336ed7e6d83266d01843df949715680ea6
shinmj0255 <shinmj0255@gmail.com>
2023-06-28 20:12:24 +0900

    jsHistoryBack, jsReplace, doDelete confirm

--- src/main/java/com/example/demo/util/Util.java
+++ src/main/java/com/example/demo/util/Util.java
@@ -2,17 +2,56 @@ package com.example.demo.util;
 
 public class Util {
 	public static boolean empty(Object obj) {
-		
+
 		if (obj == null) {
 			return true;
 		}
-		
+
 		String str = (String) obj;
-		
+
 		return str.trim().length() == 0;
 	}
-	
+
 	public static String f(String format, Object... args) {
 		return String.format(format, args);
+	}
+
+	public static String jsHistoryBack(String msg) {
+
+		if (msg == null) {
+			msg = "";
+		}
+
+		return Util.f("""
+						<script>
+							const msg = '%s'.trim();
+							if (msg.length > 0) {
+								alert(msg);
+							}
+							history.back();
+						</script>
+					""", msg);
+	}
+	
+	public static String jsReplace(String msg, String uri) {
+		
+		if (msg == null) {
+			msg = "";
+		}
+
+		if (uri == null) {
+			uri = "";
+		}
+
+		return Util.f("""
+						<script>
+							const msg = '%s'.trim();
+							if (msg.length > 0) {
+								alert(msg);
+							}
+							location.replace('%s');
+						</script>
+					""", msg, uri);
+		
 	}
 }
