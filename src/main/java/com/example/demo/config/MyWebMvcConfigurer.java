2710062ed656fdb86c54e84d57f660198b51e7d1
shinmj0255 <shinmj0255@gmail.com>
2023-06-29 20:07:20 +0900

    interceptor 구현, jsPrintHistoryBack

---
+++ src/main/java/com/example/demo/config/MyWebMvcConfigurer.java
@@ -0,0 +1,35 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.demo.interceptor.BeforeActionInterceptor;
+import com.example.demo.interceptor.NeedLoginInterceptor;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+
+	private BeforeActionInterceptor beforeActionInterceptor;
+	private NeedLoginInterceptor needLoginInterceptor;
+
+	@Autowired
+	public MyWebMvcConfigurer(BeforeActionInterceptor beforeActionInterceptor,
+			NeedLoginInterceptor needLoginInterceptor) {
+		this.beforeActionInterceptor = beforeActionInterceptor;
+		this.needLoginInterceptor = needLoginInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**");
+		
+		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/doAdd")
+		.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/doDelete");
+		
+
+
+	}
+
+}
